baa13142baf283d8698355184062866e9437c522
jgrissom <jgrissom91@gmail.com>
2021-04-04 09:44:18 -0500

    Add Customer (Company Name only) - Models/NorthwindContext: AddCustomer method - Customer/Register method that responds to HTTP POST requests

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,9 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
 
 namespace Northwind.Controllers
 {
     public class CustomerController : Controller
     {
+        // this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public CustomerController(NorthwindContext db) => _northwindContext = db;
         public IActionResult Register() => View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _northwindContext.AddCustomer(customer);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
     }
 }
